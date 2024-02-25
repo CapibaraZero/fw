811e32c45f4321837fb20a43736511a32ff89ff4
andreock <vivanco11.ac@gmail.com>
2024-02-25 17:21:58 +0100

    Prepare NFCAttacks for GUI

--- lib/nfc_attacks/nfc_attacks.cpp
+++ lib/nfc_attacks/nfc_attacks.cpp
@@ -23,6 +23,7 @@
 #include "fm.hpp"
 #include "helper.hpp"
 #include "../../include/debug.h"
+#include "ArduinoJson.h"
 
 using namespace std;
 
@@ -56,8 +57,9 @@ bool NFCAttacks::is_there_null_blocks(NFCTag *tag)
     return false;
 }
 
-void NFCAttacks::bruteforce()
+bool NFCAttacks::bruteforce()
 {
+    bruteforce_status = true;
     File keys = open(NFC_KEYS_FILE, "r");
     if (keys.available())
     {
@@ -66,6 +68,8 @@ void NFCAttacks::bruteforce()
         size_t key_index = 0;                   // Index for the current key in keys
         for (String line : lines)
         {
+            if(line[0] == '#')  // If is a comment, skip
+                continue;
             vector<String> raw_key = string_split((char *)line.c_str(), " "); // String string for each space
             uint8_t parsed_key[6];
             size_t i = 0; // byte index counter
@@ -75,22 +79,23 @@ void NFCAttacks::bruteforce()
                 SERIAL_DEVICE.printf("%s\n", key_byte.c_str());
             }
 
-            uint8_t *uid;
-            uint8_t *uid_length;
+            uint8_t uid[7];
+            uint8_t uid_length;
 
-            while (!nfc_framework.get_tag_uid(uid, uid_length))
-            {
-                __asm__ __volatile__("nop\n\t");
-            };
+            nfc_framework.get_tag_uid(uid, &uid_length);
 
             LOG_INFO("Found a new tag!\n");
             LOG_INFO("Tag UID: ");
-            printHex(uid);
-            NFCTag tag = dump_tag(parsed_key);
-            if (!is_there_null_blocks(&tag))
+            nfc_framework.printHex(uid, 7);
+
+            tried_keys++;   // For statistics use
+            DumpResult dump_result;
+            NFCTag tag = dump_tag(parsed_key, &dump_result);
+            if (dump_result.unauthenticated == 0)
             {
                 LOG_SUCCESS("Key found!");
-                return;
+                bruteforce_status = false;
+                return true;
             }
             key_index++; // Go to next key
         }
@@ -99,28 +104,34 @@ void NFCAttacks::bruteforce()
     {
         LOG_ERROR("Keys file not found.\n");
     }
+    bruteforce_status = false;
+    return false;
 }
 
 void NFCAttacks::read_uid(uint8_t *uid, uint8_t *uid_length)
 {
+    Serial0.println("Read UID: ");
     if (!nfc_framework.get_tag_uid(uid, uid_length))
     {
         uid = NULL;
         uid_length = NULL;
     };
 }
-
-NFCTag NFCAttacks::dump_tag()
+NFCTag NFCAttacks::dump_tag(DumpResult *result)
 {
     uint8_t key_universal[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}; // Universal key
     size_t key_length;
-    return NFCTag(nfc_framework.dump_tag(key_universal, &key_length), key_length); // uint8_t to NFCTag object
+    uint8_t *data = nfc_framework.dump_tag(key_universal, &key_length, result);
+    if(key_length == 4)
+        return NFCTag(data, key_length); // uint8_t to NFCTag object
+    else 
+        return NFCTag(data, key_length, NTAG213_PAGES); // uint8_t to NFCTag object
 }
 
-NFCTag NFCAttacks::dump_tag(uint8_t *key)
+NFCTag NFCAttacks::dump_tag(uint8_t *key, DumpResult *result)
 {
     size_t key_length;
-    return NFCTag(nfc_framework.dump_tag(key, &key_length), key_length); // uint8_t to NFCTag object
+    return NFCTag(nfc_framework.dump_tag(key, &key_length, result), key_length); // uint8_t to NFCTag object
 }
 
 NFCTag NFCAttacks::dump_ntag(int pages)
@@ -128,42 +139,60 @@ NFCTag NFCAttacks::dump_ntag(int pages)
     return NFCTag(nfc_framework.dump_ntag2xx_tag(pages), 7, pages);
 }
 
-void NFCAttacks::write_tag(NFCTag *tag)
+uint8_t NFCAttacks::write_tag(NFCTag *tag)
 {
     uint8_t key_universal[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}; // Universal key
-    for (size_t i = 0; i < tag->get_blocks_count(); i++)
-    {
-        Serial0.printf("Writing block: %i\n", i);
-        uint8_t block[BLOCK_SIZE] = {0};
-        tag->get_block(i, block);
-        if (nfc_framework.write_tag(i, block, key_universal))
+    uint8_t unwritable = 0;
+    uint8_t uid[7];
+    uint8_t uid_length;
+    if(nfc_framework.get_tag_uid(uid, uid_length)) {
+        for (size_t i = 0; i < tag->get_blocks_count(); i++)
         {
-            LOG_SUCCESS("Block written correctly\n");
+            Serial0.printf("Writing block: %i\n", i);
+            uint8_t block[BLOCK_SIZE] = {0};
+            tag->get_block(i, block);
+            if (nfc_framework.write_tag(i, block, key_universal))
+            {
+                LOG_SUCCESS("Block written correctly\n");
+            }
+            else
+            {
+                unwritable++;
+                LOG_ERROR("Error during writing block.");
+            };
         }
-        else
-        {
-            LOG_ERROR("Error during writing block.");
-        };
+    } else {
+        Serial0.println("Unable to find card");
     }
+    return unwritable;
 }
 
-void NFCAttacks::write_tag(NFCTag *tag, int starting_block)
+uint8_t NFCAttacks::write_tag(NFCTag *tag, int starting_block)
 {
     uint8_t key_universal[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}; // Universal key
-    for (size_t i = starting_block; i < tag->get_blocks_count(); i++)
-    {
-        Serial0.printf("Writing block: %i\n", i);
-        uint8_t block[BLOCK_SIZE] = {0};
-        tag->get_block(i, block);
-        if (nfc_framework.write_tag(i, block, key_universal))
+    uint8_t unwritable = 0;
+    uint8_t uid[7];
+    uint8_t uid_length;
+    // Avoid crashing if no card is present
+    if(nfc_framework.get_tag_uid(uid, uid_length)) {
+        for (size_t i = starting_block; i < tag->get_blocks_count(); i++)
         {
-            LOG_SUCCESS("Block written correctly\n");
+            Serial0.printf("Writing block: %i\n", i);
+            uint8_t block[BLOCK_SIZE] = {0};
+            tag->get_block(i, block);
+            if (nfc_framework.write_tag(i, block, key_universal))
+            {
+                LOG_SUCCESS("Block written correctly\n");
+            }
+            else
+            {
+                LOG_ERROR("Error during writing block.");
+            };
         }
-        else
-        {
-            LOG_ERROR("Error during writing block.");
-        };
+    } else {
+        Serial0.println("Unable to find card");
     }
+    return unwritable;
 }
 
 void NFCAttacks::write_tag(NFCTag *tag, uint8_t *key)
@@ -202,12 +231,12 @@ void NFCAttacks::write_tag(NFCTag *tag, uint8_t *key, int starting_block)
     }
 }
 
-void NFCAttacks::format_tag(bool ultralight)
+uint8_t NFCAttacks::format_tag(bool ultralight)
 {
     LOG_INFO("Formatting tag...");
     uint8_t empty_tag_data[ultralight ? MIFARE_ULTRALIGHT_SIZE : MIFARE_CLASSIC_SIZE] = {0};
     NFCTag empty_tag = NFCTag(empty_tag_data, 4);   // Uid lenght is dummy here since it's an empty key
-    write_tag(&empty_tag, ultralight ? 7 : 4);
+    return write_tag(&empty_tag, ultralight ? 7 : 4);
 }
 
 void NFCAttacks::format_tag(uint8_t *key ,bool ultralight)
@@ -219,34 +248,42 @@ void NFCAttacks::format_tag(uint8_t *key ,bool ultralight)
 }
 
 
-void NFCAttacks::write_ntag(NFCTag *tag)
+uint8_t NFCAttacks::write_ntag(NFCTag *tag)
 {
     const size_t tag_size = tag->get_blocks_count();
     const size_t reserved_pages = tag_size == NTAG203_PAGES ? NTAG203_RESERVED_PAGES : NTAG21X_RESERVED_PAGES;
-
-    /* Skip non-user sectors since it's not writable */
-    for (size_t i = 4; i != tag_size - reserved_pages; i++)
-    {
-        Serial0.printf("Writing block: %i\n", i);
-        uint8_t block[4];
-        tag->get_block(i, block);
-        if (nfc_framework.write_ntag2xx_page(i, block))
+    uint8_t unwritable = 0;
+    uint8_t uid[7];
+    uint8_t uid_length;
+    if(nfc_framework.get_tag_uid(uid, uid_length)) {
+        /* Skip non-user sectors since it's not writable */
+        for (size_t i = 4; i != tag_size - reserved_pages; i++)
         {
-            LOG_SUCCESS("Block written correctly\n");
+            Serial0.printf("Writing block: %i\n", i);
+            uint8_t block[4];
+            tag->get_block(i, block);
+            if (nfc_framework.write_ntag2xx_page(i, block))
+            {
+                LOG_SUCCESS("Block written correctly\n");
+            }
+            else
+            {
+                unwritable++;
+                LOG_ERROR("Error during writing block.\n");
+            };
         }
-        else
-        {
-            LOG_ERROR("Error during writing block.\n");
-        };
+    } else {
+        Serial0.println("Unable to find card");
     }
+    return unwritable;
 }
 
-void NFCAttacks::format_ntag(int pages)
+uint8_t NFCAttacks::format_ntag(int pages)
 {
     LOG_INFO("Formatting ntag...");
     uint8_t empty_tag_data[pages * NTAG_PAGE_SIZE] = {0};
     NFCTag empty_tag = NFCTag(empty_tag_data, 7, pages);
-    write_ntag(&empty_tag);
+    return write_ntag(&empty_tag);
 }
 
 bool NFCAttacks::detect_felica(uint8_t *idm, uint8_t *pmm, uint16_t *sys_code) {
@@ -274,4 +311,24 @@ bool NFCAttacks::felica_write(uint8_t service_codes_list_length, uint16_t *servi
 bool NFCAttacks::felica_write(uint8_t block_number, uint16_t *block_list, uint8_t data[][16]) {
     uint16_t default_service_code[1] = { 0x0009 };   // Default service code for writing. Should works for every card
     return nfc_framework.felica_write_without_encryption(1, default_service_code,block_number, block_list, data);
+}
+
+#define NFC_CONFIG_FILE "/nfc/config.json"
+#define TAG_SIZE(length) length > 4 ? MIFARE_ULTRALIGHT_SIZE : MIFARE_CLASSIC_SIZE    // Ultralight have uid bit length and 512 bytes
+
+NFCTag NFCAttacks::get_tag_towrite(uint8_t uid_length) {
+    File nfc_config = open(NFC_CONFIG_FILE, "r");
+    StaticJsonDocument<256> doc;
+    DeserializationError error = deserializeJson(doc, nfc_config);
+    uint8_t empty[1024];
+    if (error) {
+        LOG_ERROR("Invalid NFC configuration");
+        return NFCTag(empty, 7);
+    }
+    // if uid_length == 7 search ntag
+    File nfc_saved_tag = open(doc["ISO14443A"]["ntag_ultralight"]["tag_to_write"], "r");
+    const size_t tag_size = TAG_SIZE(uid_length);
+    byte buffer[tag_size] = { 0 };
+    nfc_saved_tag.read(buffer, tag_size);
+    return NFCTag(buffer, uid_length, NTAG213_PAGES);
 }
--- lib/nfc_attacks/nfc_attacks.hpp
+++ lib/nfc_attacks/nfc_attacks.hpp
@@ -27,22 +27,25 @@ private:
     NFCFramework nfc_framework = NFCFramework();
     bool is_there_null_blocks(NFCTag *tag);
     // For internal usage(format)
-    void write_tag(NFCTag *tag, int starting_block);
+    uint8_t write_tag(NFCTag *tag, int starting_block);
     void write_tag(NFCTag *tag, uint8_t *key, int starting_key);
+    NFCTag *current_tag = (NFCTag *)malloc(sizeof(NFCTag));
+    uint8_t tried_keys = 0;
+    bool bruteforce_status = true;
 public:
     NFCAttacks(/* args */);
-    inline ~NFCAttacks() {};
-    void bruteforce();
+    ~NFCAttacks() {};
+    bool bruteforce();
     void read_uid(uint8_t *uid, uint8_t *uid_length);
-    NFCTag dump_tag();
-    NFCTag dump_tag(uint8_t *key);
+    NFCTag dump_tag(DumpResult *result);
+    NFCTag dump_tag(uint8_t *key, DumpResult *result);
     NFCTag dump_ntag(int pages);
-    void write_tag(NFCTag *tag);
+    uint8_t write_tag(NFCTag *tag);
     void write_tag(NFCTag *tag, uint8_t *key);
-    void format_tag(bool ultralight = false);
+    uint8_t format_tag(bool ultralight = false);
     void format_tag(uint8_t *key, bool ultralight = false);
-    void write_ntag(NFCTag *tag);
-    void format_ntag(int pages);
+    uint8_t write_ntag(NFCTag *tag);
+    uint8_t format_ntag(int pages);
     bool detect_felica(uint8_t *idm, uint8_t *pmm, uint16_t *sys_code);
     bool felica_read(uint8_t service_length, uint16_t *service_codes, uint8_t num_blocks, 
                             uint16_t *block_list, uint8_t data[][16]);
@@ -51,6 +54,15 @@ public:
                             uint8_t block_number, uint16_t *block_list, uint8_t data[][16]);
     bool felica_write(uint8_t block_number, uint16_t *block_list, uint8_t data[][16]);
     void printHex(uint8_t *payload) { nfc_framework.printHex(payload, sizeof(payload) / sizeof(payload[0])); }
+    void printHex(uint8_t *payload, size_t size) { nfc_framework.printHex(payload, size); }
+
+    void set_scanned_tag(NFCTag *tag) {
+        memcpy(current_tag, tag, sizeof(tag));
+    };
+    NFCTag *get_scanned_tag() { return current_tag; };
+    NFCTag get_tag_towrite(uint8_t uid_length);
+    uint8_t get_tried_keys() { return tried_keys; };
+    bool get_bruteforce_status() { return bruteforce_status; };
 };
 
 #endif
