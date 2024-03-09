96c318aca74d584e3d3fe0c7d31424fc9a7815e1
andreock <vivanco11.ac@gmail.com>
2024-03-09 23:13:29 +0100

    Make sdcard_helper list all fs/folder

--- lib/sdcard_helper/sdcard_helper.cpp
+++ lib/sdcard_helper/sdcard_helper.cpp
@@ -10,11 +10,25 @@ list<string> list_dir(File root)
     while (file)
     {
         if (!file.isDirectory())
-        {
             files.push_back(file.name());
-        }else {
+        else
             files.push_back((string)file.name() + "/"); // A folder is indicated by a final slash. Cast char* to string to use +
-        }
+        file = root.openNextFile();
+    }
+
+    return files;
+}
+
+list<string> list_fs(File root)
+{
+    list<string> files = list<string>();
+    File file = root.openNextFile();
+    while (file)
+    {
+        if (!file.isDirectory())
+            files.push_back(file.path());
+        else 
+            files.splice(files.end(), list_dir(file));
         file = root.openNextFile();
     }
 
