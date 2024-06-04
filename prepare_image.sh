2e414c68544911fb82ec24cc0c83a8ac4b2d2917
Andrea Canale <vivanco11.ac@gmail.com>
2024-06-05 00:28:56 +0200

    Enhance prepare image script

--- prepare_image.sh
+++ prepare_image.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 # A simple script to extract build images from pio folder
+BOARD=$1
+mkdir build
 
 cp /Users/andreock/.platformio/packages/framework-arduinoespressif32/tools/partitions/boot_app0.bin build/
-cp .pio/build/esp32-s3-devkitc-1/partitions.bin build/
-cp .pio/build/esp32-s3-devkitc-1/firmware.bin build/
-cp .pio/build/esp32-s3-devkitc-1/bootloader.bin build/
+cp .pio/build/"$BOARD"/partitions.bin build/
+cp .pio/build/"$BOARD"/firmware.bin build/
+cp .pio/build/"$BOARD"/bootloader.bin build/
+
+cd build && zip -9 -r capibaraZero_0_2_0_"$BOARD".zip *
