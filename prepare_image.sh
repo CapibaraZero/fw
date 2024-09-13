975848b31001940a00a05d878a6225e2ac30bbfe
andreock <andreockx@proton.me>
2024-09-13 17:07:52 +0200

    Update prepere image script

--- prepare_image.sh
+++ prepare_image.sh
@@ -4,9 +4,14 @@
 BOARD=$1
 mkdir build
 
-cp /Users/andreock/.platformio/packages/framework-arduinoespressif32/tools/partitions/boot_app0.bin build/
+# Remove old builds
+rm -rf build/*
+
+pio run -e "$BOARD"
+
+cp /home/"$(whoami)"/.platformio/packages/framework-arduinoespressif32/tools/partitions/boot_app0.bin build/
 cp .pio/build/"$BOARD"/partitions.bin build/
 cp .pio/build/"$BOARD"/firmware.bin build/
 cp .pio/build/"$BOARD"/bootloader.bin build/
 
-cd build && zip -9 -r capibaraZero_0_2_0_"$BOARD".zip *
+cd build && zip -9 -r capibaraZero_0_5_0_"$BOARD".zip *
