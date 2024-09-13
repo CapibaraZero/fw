06d393ecd44ce74bb4a3f4c80aa917222d723b8d
andreock <andreockx@proton.me>
2024-09-14 00:23:20 +0200

    Update format script

--- format.sh
+++ format.sh
@@ -7,6 +7,7 @@ files=$(find . -type f)
 for file in $files
 do
 if [[ $file != *".git"* && $file != *".vscode"* && $file != *".pio"* && $file != *"Library"* && $file != *"BusIO"*  && $file != *".csv"* && ($file == *".c"* || $file == *".h"*) ]]; then
+    echo "Formatting $file"
     clang-format -style=Google -i "$file"
 fi
 done
