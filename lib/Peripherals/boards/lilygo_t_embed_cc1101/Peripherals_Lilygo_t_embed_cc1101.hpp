ccbb001e38bf652cccd73ff58edbf9d1311cc54c
andreock <andreockx@proton.me>
2024-11-30 11:45:36 +0100

    Use rising interrupt for ok btn of Lilygo T-Embed CC1101

--- lib/Peripherals/boards/lilygo_t_embed_cc1101/Peripherals_Lilygo_t_embed_cc1101.hpp
+++ lib/Peripherals/boards/lilygo_t_embed_cc1101/Peripherals_Lilygo_t_embed_cc1101.hpp
@@ -76,7 +76,7 @@ class Peripherals_Lilygo_t_embed_cc1101 : public Peripherals {
   };
   void init_navigation() {
     init_rotary_encoder();
-    init_navigation_btn(OK_BTN_PIN, handle_ok_button, INPUT, HIGH);
+    init_navigation_btn(OK_BTN_PIN, handle_ok_button, INPUT, RISING);
   };
 };
 
