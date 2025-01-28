ac75541b9d2316592d3dae2275b0588463665e79
andreock <andreockx@proton.me>
2025-01-29 00:51:16 +0100

    Fix action attemp 1

--- prepare_image.sh
+++ prepare_image.sh
@@ -3,16 +3,16 @@
 # A simple script to extract firmware images from pio folder
 BOARD=$1
 VERSION="0_5_2"
-mkdir firmware
+# mkdir firmware
 
 # Remove old firmware
-rm -rf firmware/*
+# rm -rf firmware/*
 
 # pio run -e "$BOARD"
 
 cp /home/"$(whoami)"/.platformio/packages/framework-arduinoespressif32/tools/partitions/boot_app0.bin firmware/"$BOARD"
-cp .pio/firmware/"$BOARD"/partitions.bin firmware/"$BOARD"
-cp .pio/firmware/"$BOARD"/firmware.bin firmware/"$BOARD"
-cp .pio/firmware/"$BOARD"/bootloader.bin firmware/"$BOARD"
+cp .pio/build/"$BOARD"/partitions.bin firmware/"$BOARD"
+cp .pio/build/"$BOARD"/firmware.bin firmware/"$BOARD"
+cp .pio/build/"$BOARD"/bootloader.bin firmware/"$BOARD"
 
 # cd firmware && zip -9 -r capibaraZero_"$VERSION"_"$BOARD".zip *
